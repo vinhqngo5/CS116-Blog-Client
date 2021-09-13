74d0011467596c3052b35fb8c5686fd76043b4a0
vinhqngo5 <vinhqngo5@gmail.com>
2021-09-13 09:20:15 +0700

    Init app layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-function App() {
-	return <div className="App"></div>;
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Header from "./components/layout/header/Header";
+import Footer from "./components/layout/footer/Footer";
+export default function App() {
+	return (
+		<div>
+			<Router>
+				<Header></Header>
+				<Switch>
+					<Route exact path="/">
+						<Home></Home>
+					</Route>
+				</Switch>
+				<Footer></Footer>
+			</Router>
+		</div>
+	);
 }
-
-export default App;
---
+++ src/components/layout/rightSideBar/RightSideBar.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function RightSideBar() {
+	return <div>RightSideBar</div>;
+}
